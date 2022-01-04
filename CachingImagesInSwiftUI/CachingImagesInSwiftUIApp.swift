0f0e50a35aa8421988f7450e714f3f802591b5f4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2022-01-04 10:02:27 +0200

    Initial Commit

---
+++ CachingImagesInSwiftUI/CachingImagesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingImagesInSwiftUIApp.swift
+//  CachingImagesInSwiftUI
+//
+//  Created by Ramill Ibragimov on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CachingImagesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
